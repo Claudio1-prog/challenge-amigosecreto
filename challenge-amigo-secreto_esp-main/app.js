8d992344bdd881d1065092354759b81cd296cbcb
Ignacio Segovia <nachomtb1400@gmail.com>
2025-08-08 00:19:49 -0300

    Agrego validaciones y función sortear con reinicio + README

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,7 +2,7 @@
 
 let amigos = []
 
-function agregarAmigos() {
+function agregarAmigo() {
     let input = document.getElementById("amigo");
     let nombre = input.value.trim()
 
@@ -10,10 +10,49 @@ function agregarAmigos() {
         alert("Por favor, inserte un nombre");
         return;
     }
+    const soloLetras = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    if (!soloLetras.test(nombre)) {
+        alert("El nombre solo puede contener letras");
+        return;
+    }
     amigos.push(nombre);
+    console.log(amigos);
     input.value = "";
+    mostarAmigos();
+}
+
+
+
+function mostarAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let amigo of amigos) {
+        lista.innerHTML += `<li>${amigo}</li>`;
+    }
 
 }
+
+
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo sorteado es: ${amigoSorteado}</li>`;
+    amigos = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    setTimeout(() => {
+        resultado.innerHTML = "";
+    }, 3000);
+
+
+}
+
 
 
 
